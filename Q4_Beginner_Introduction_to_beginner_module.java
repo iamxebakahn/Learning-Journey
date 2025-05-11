d0a5ba2a6e8c0a514aa7ba157320cc511f43a244
iamxebakahn <zebadwight@gmail.com>
2025-05-11 19:26:25 +0530

    Update Q4_Beginner_Introduction_to_beginner_module.java

--- Q4_Beginner_Introduction_to_beginner_module.java
+++ Q4_Beginner_Introduction_to_beginner_module.java
@@ -1,3 +1,28 @@
+// Problem Description
+
+// Print the following pattern in output
+
+// *****  
+// ****  
+// ***  
+// **  
+// *
+// Output Format
+
+// Print the following pattern in 5 lines with 5, 4, 3, 2, 1 stars in each line
+
+// *****  
+// ****  
+// ***  
+// **  
+// *
+// Example Output
+
+// *****  
+// ****  
+// ***  
+// **  
+// *
 import java.lang.*;
 import java.util.*;
 
