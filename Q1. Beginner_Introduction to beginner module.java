d95b95e65e366609bb92ce33d97cc7ebe9a7c308
iamxebakahn <zebadwight@gmail.com>
2025-05-11 18:26:06 +0530

    Update Q1. Beginner_Introduction to beginner module.java

--- Q1. Beginner_Introduction to beginner module.java
+++ Q1. Beginner_Introduction to beginner module.java
@@ -1,3 +1,13 @@
+// Problem Description
+// Print the first five letters of the English alphabet i.e. A, B, C, D and E.
+// Output Format
+// Print the characters in separate lines.
+// Example Output
+// A
+// B
+// C
+// D
+// E
 public class Main {
 
     public static void main(String[] args) {
