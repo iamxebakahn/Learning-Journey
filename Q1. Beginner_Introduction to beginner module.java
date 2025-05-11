e28d98c68d6b2397923168a4de714394ad8e9e3b
iamxebakahn <zebadwight@gmail.com>
2025-05-11 18:24:57 +0530

    Add files via upload

---
+++ Q1. Beginner_Introduction to beginner module.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi");
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+    }
+}
