67e432ea19f41d32e24f18702e100f187c9ccf9b
iamxebakahn <zebadwight@gmail.com>
2025-05-11 18:51:37 +0530

    Update Q2_Beginner_Introduction_to_beginner_module.java

--- Q2_Beginner_Introduction_to_beginner_module.java
+++ Q2_Beginner_Introduction_to_beginner_module.java
@@ -1,3 +1,16 @@
+// Problem Description
+// Print "Hello World !" in the output.
+
+// Output Format
+
+// Print in a single line "Hello World !"
+
+
+// Example Output
+
+// Hello World !
+
+// Note - You don't have to print "" in the output
 public class Q2_Beginner_Introduction_to_beginner_module{
     public static void main(String[] args) 
     {
