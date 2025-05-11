58557dc12dd837bbeec1300a7d8ce71de6de10fb
iamxebakahn <zebadwight@gmail.com>
2025-05-11 18:50:54 +0530

    Add files via upload

---
+++ Q2_Beginner_Introduction_to_beginner_module.java
@@ -0,0 +1,12 @@
+public class Q2_Beginner_Introduction_to_beginner_module{
+    public static void main(String[] args) 
+    {
+
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+       
+        System.out.println("Hello World !");
+    }
+}
