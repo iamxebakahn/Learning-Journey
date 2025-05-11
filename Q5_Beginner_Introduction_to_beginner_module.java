b9a84625f4f7bf03136705a89c8cf293db90d81a
iamxebakahn <zebadwight@gmail.com>
2025-05-11 19:29:26 +0530

    Add files via upload

---
+++ Q5_Beginner_Introduction_to_beginner_module.java
@@ -0,0 +1,29 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q5_Beginner_Introduction_to_beginner_module{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+               {
+                    
+                    if(j==i)
+                    {
+                        for(int k=5;k>=i;k--)
+                          System.out.print("*");
+                    }
+                    else
+                    System.out.print(" ");
+               }
+
+            System.out.println();
+        }
+        
+
+    }
+}
