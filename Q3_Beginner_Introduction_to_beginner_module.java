b2e4c97ab462d757c16ebd9c7d3bf4e9597231ce
iamxebakahn <zebadwight@gmail.com>
2025-05-11 19:22:13 +0530

    Add files via upload

---
+++ Q3_Beginner_Introduction_to_beginner_module.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q3_Beginner_Introduction_to_beginner_module{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        System.out.println(6+3);
+        System.out.println(6-3);
+        System.out.println(6*3);
+        System.out.println(6.0/3.0);
+    }
+}
