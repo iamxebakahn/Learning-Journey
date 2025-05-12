942cb59a8a182b95b470186692d189cf7180be18
iamxebakahn <zebadwight@gmail.com>
2025-05-12 13:00:31 +0530

    Add files via upload

---
+++ Q5AP_Beginner_Introduction_to_beginner_module.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q5AP_Beginner_Introduction_to_beginner_module{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        
+        System.out.println("Dear Compiler");
+        System.out.println("----Never Give me bugs");
+        System.out.println("----If bugs are there, please help me to fix It");
+        System.out.println("----Thank You");
+       
+
+
+
+    }
+}
