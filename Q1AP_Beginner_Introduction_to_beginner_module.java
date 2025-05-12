93c154d3eb255595cc570f322de6aa17ee69759e
iamxebakahn <zebadwight@gmail.com>
2025-05-12 12:44:07 +0530

    Add files via upload

---
+++ Q1AP_Beginner_Introduction_to_beginner_module.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q1AP_Beginner_Introduction_to_beginner_module{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        
+        for(int i=5;i>=1;i--)
+        {
+            if((i==5)||(i==1))
+            {
+                for(int j=1;j<=6;j++)
+                    System.out.print("*");
+            }
+            else 
+                System.out.print("*");
+            
+            System.out.println();
+            
+        } 
+
+      
+
+    }
+}
