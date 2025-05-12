418c5d8f1f26a1cec2f21ab5e12edfd36dd8f82f
iamxebakahn <zebadwight@gmail.com>
2025-05-12 12:56:30 +0530

    Add files via upload

---
+++ Q4AP_Beginner_Introduction_to_beginner_module.java
@@ -0,0 +1,41 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q4AP_Beginner_Introduction_to_beginner_module.java{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        for(int i=1;i<=7;i++)
+        {
+            if((i==1)||(i==4)||(i==7))
+            {
+                for(int j=1;j<=6;j++)
+                    System.out.print("*");
+            }
+
+            else if((i==2)||(i==3))
+            {
+                System.out.print("*");
+            }
+
+            else
+            {
+                for(int k=1;k<=6;k++)
+                {
+                    if(k==6)
+                        System.out.print("*");
+                    else
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+
+
+
+
+    }
+}
