84cb2edeca1ca9be8302e0ecd1208cde4d30a98f
iamxebakahn <zebadwight@gmail.com>
2025-05-12 12:45:00 +0530

    Update Q1AP_Beginner_Introduction_to_beginner_module.java

--- Q1AP_Beginner_Introduction_to_beginner_module.java
+++ Q1AP_Beginner_Introduction_to_beginner_module.java
@@ -1,3 +1,46 @@
+// Problem Description
+
+// Print the following Pattern in Output :
+// ******
+// *
+// *
+// *
+// ******
+
+
+// Problem Constraints
+
+// No contstraints
+
+
+// Input Format
+
+// No need to take input.
+
+
+// Output Format
+
+// Print the following pattern in 5 lines with 6 stars in first and last line, and 1 star in 2nd, 3rd, and 4th line.
+// ******
+// *
+// *
+// *
+// ******
+
+
+// Example Input
+
+// No input required.
+
+
+// Example Output
+
+// Output :
+// ******
+// *
+// *
+// *
+// ******
 import java.lang.*;
 import java.util.*;
 
